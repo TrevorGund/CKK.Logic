d3dd3023d28afb1e878a7bdf23ef46a860ab7ebd
Trevor Gunderson <trevorgund@gmail.com>
2023-03-01 23:25:34 -0700

    Project Deliverable 2

---
+++ CKK.Logic/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿namespace CKK.Logic.Models
+{
+    class Customer
+    {
+        private int _id;
+        private string _name;
+        private string _address;
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public void Address(string address)
+        {
+            _address = address;
+        }
+    }
+}
