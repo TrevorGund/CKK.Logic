a63ad7fb6900c0da526cb6f04744a90fb688a85a
Trevor Gunderson <trevorgund@gmail.com>
2023-03-09 14:46:59 -0700

    PD3 Correction

--- CKK.Logic/Models/Store.cs
+++ CKK.Logic/Models/Store.cs
@@ -28,6 +28,7 @@
             _name = name;
         }
 
+
         public void AddStoreItem(Product prod)
         {
             if (Product1 == null)
@@ -42,70 +43,65 @@
             {
                 Product3 = prod;
             }
-            else if (Product3!= null)
+            else if (Product3 != null)
             {
                 Product3 = prod;
             }
-
-            public void RemoveStoreItem(int productNumber)
-            {
-                if (productNumber == 1)
-                {
-                    Product1 = null;
-                }
-                else if (productNumber == 2)
-                {
-                    Product2 = null;
-                }
-                else if(productNumber == 3)
-                {
-                    Product3 = null;
-                }
-
-                public Product GetStoreItem(int productNumber)
-                {
-                    if(productNumber == 1)
-                    {
-                        return Product1;
-                    }
-                    else if(productNumber == 2)
-                    {
-                        return Product2;
-                    }
-                    else if(productNumber == 3)
-                    {
-                        return Product3;
-                    }
-                    else
-                    {
-                        return null;
-                    }
-
-                    public Product FindStoreItemById(int id)
-                    {
-                        if(id == Product1._id)
-                        {
-                            return Product1;
-                        }
-                        else if(id == Product2._id)
-                        {
-                            return Product2;
-                        }
-                        else if(id == Product3._id)
-                        {
-                            return Product3;
-                        }
-                        else
-                        {
-                            return null;
-                        }
-                    }
-                }
-
-            }
-            
         }
-
-
+        public void RemoveStoreItem(int productNumber)
+        {
+            if (productNumber == 1)
+            {
+                Product1 = null;
+            }
+            else if (productNumber == 2)
+            {
+                Product2 = null;
+            }
+            else if (productNumber == 3)
+            {
+                Product3 = null;
+            }
+        }
+        public Product GetStoreItem(int productNumber)
+        {
+            if (productNumber == 1)
+            {
+                return Product1;
+            }
+            else if (productNumber == 2)
+            {
+                return Product2;
+            }
+            else if (productNumber == 3)
+            {
+                return Product3;
+            }
+            else
+            {
+                return null;
+            }
+        }
+             
+        public Product FindStoreItemById(int id)
+        {
+            if(id == Product1._id)
+            {
+                return Product1;
+            }
+            else if(id == Product2._id)
+            {
+                return Product2;
+            }
+            else if(id == Product3._id)
+            {
+                return Product3;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
+
 }
